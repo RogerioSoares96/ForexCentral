683a696ff98cc4c93e6c877230f3900c2d183f15
Rogerio Barros Soares <70479058+RogerioSoares96@users.noreply.github.com>
2022-12-07 16:17:19 +0000

    Delete tradercentral directory

--- tradercentral/static/animation.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const numberOfSlides = document.querySelectorAll('.slide').length;
-    const pixelSpeed = numberOfSlides * 35
-    const pageRoot = document.querySelector(':root');
-    pageRoot.style.setProperty('--numberOfItems', numberOfSlides);
-    pageRoot.style.setProperty('--pixelSpeed', `-${pixelSpeed}px`);
-});
